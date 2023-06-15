1f953229b2a6c9659bf08b3f87d8829bf204fde3
sigma <madarasig@yandex.ru>
2023-06-15 19:30:02 +0300

    fix: covered

--- src/main/java/ru/practicum/shareit/exceptions/ItemAlreadyExistException.java
+++ src/main/java/ru/practicum/shareit/exceptions/ItemAlreadyExistException.java
@@ -1,5 +1,8 @@
 package ru.practicum.shareit.exceptions;
 
+import lombok.Generated;
+
+@Generated
 public class ItemAlreadyExistException extends RuntimeException {
     public ItemAlreadyExistException(String message) {
         super(message);
--- src/main/java/ru/practicum/shareit/exceptions/UserAlreadyExistException.java
+++ src/main/java/ru/practicum/shareit/exceptions/UserAlreadyExistException.java
@@ -1,6 +1,8 @@
 package ru.practicum.shareit.exceptions;
 
+import lombok.Generated;
 
+@Generated
 public class UserAlreadyExistException extends RuntimeException {
     public UserAlreadyExistException(String message) {
         super(message);
